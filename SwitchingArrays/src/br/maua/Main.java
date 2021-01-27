04d25ff2805096baae631e55519c66a4ea488a29
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-01-27 17:33:37 -0300

    Correção

--- SwitchingArrays/src/br/maua/Main.java
+++ SwitchingArrays/src/br/maua/Main.java
@@ -24,29 +24,32 @@ public class Main {
 
     }
 
-    public int Solve(int[] arr) {
-        if ( arr.length == 1 ) return 1;
+    public class Solve {
+        public int Solve(int[] arr) {
+            if ( arr.length == 1 ) return 1;
 
-        int even = arr[0];
-        int odd = arr[1];
-        int start = 0;
-        int maxLen = 0;
+            int even = arr[0];
+            int odd = arr[1];
+            int start = 0;
+            int maxLen = 0;
 
-        for ( int i = 2; i < arr.length; ++i ) {
+            for ( int i = 2; i < arr.length; ++i ) {
 
-            if ( (i % 2 == 0 && arr[i] != even) || (i % 2 == 1 && arr[i] != odd) ) {
-                maxLen = Math.max(maxLen, i - start);
-                start = i - 1;
-                if ( i % 2 == 0 ) {
-                    even = arr[i];
-                    odd = arr[i - 1];
-                } else {
-                    even = arr[i - 1];
-                    odd = arr[i];
+                if ( (i % 2 == 0 && arr[i] != even) || (i % 2 == 1 && arr[i] != odd) ) {
+                    maxLen = Math.max(maxLen, i - start);
+                    start = i - 1;
+                    if ( i % 2 == 0 ) {
+                        even = arr[i];
+                        odd = arr[i - 1];
+                    } else {
+                        even = arr[i - 1];
+                        odd = arr[i];
+                    }
                 }
             }
-        }
 
-        return Math.max(maxLen, arr.length - start);
+            return Math.max(maxLen, arr.length - start);
+        }
     }
+
 }
