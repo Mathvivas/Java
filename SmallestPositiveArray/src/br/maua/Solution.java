6362c3ace3cc34946ce57020227442e2574b6a23
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-01-27 12:43:07 -0300

    Classe Solution

---
+++ SmallestPositiveArray/src/br/maua/Solution.java
@@ -0,0 +1,25 @@
+package br.maua;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public int solution(int[] arr) {
+        int n = arr.length;
+        Set<Integer> set = new HashSet<>();
+
+        for ( int item : arr ) {
+            if (item > 0) {
+                set.add(item);
+            }
+        }
+
+        for ( int i = 1; i <= n; i++ ) {
+            if ( !set.contains(i) ) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
