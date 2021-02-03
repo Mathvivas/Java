1a7e97d6082a1dbf3d43f15b309086a53d53b6c5
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-03 18:29:00 -0300

    Update Vector

--- collections/src/main/java/br/maua/list/ExemploVector.java
+++ collections/src/main/java/br/maua/list/ExemploVector.java
@@ -1,8 +1,22 @@
 package br.maua.list;
 
+import java.util.List;
+import java.util.Vector;
+
 public class ExemploVector {
 
     public static void main(String[] args) {
-        
+        List<String> esportes = new Vector<>();
+
+        esportes.add("Futebol");
+        esportes.add("Basquete");
+        esportes.add("Tênis de Mesa");
+        esportes.add("Volêi");
+
+        System.out.println(esportes.get(2));
+
+        for( String esporte: esportes ) {
+            System.out.println(esporte);
+        }
     }
 }
