616e47330954d3fb3498246f1fc28016f8b3f86a
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-04 13:50:17 -0300

    Update HashTable

--- collections/src/main/java/br/maua/map/ExemploHashTable.java
+++ collections/src/main/java/br/maua/map/ExemploHashTable.java
@@ -1,6 +1,7 @@
 package br.maua.map;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 /*
@@ -13,6 +14,37 @@ import java.util.Map;
 public class ExemploHashTable {
 
     public static void main(String[] args) {
+        Hashtable<String, Integer> estudantes = new Hashtable<>();
 
+        estudantes.put("Carlos", 33);
+        estudantes.put("Mariana", 21);
+        estudantes.put("Rafaela", 18);
+        estudantes.put("Pedro", 44);
+
+        System.out.println(estudantes);
+
+        // Atualiza a chave
+        estudantes.put("Pedro", 55);
+
+        System.out.println(estudantes);
+
+        estudantes.remove("Pedro");
+
+        System.out.println(estudantes);
+
+        int idade = estudantes.get("Mariana");
+        System.out.println(idade);
+
+        System.out.println("size = " + estudantes.size());
+
+        // Navega nos registros
+        for ( Map.Entry<String, Integer> entry : estudantes.entrySet() ) {
+            System.out.println(entry.getKey() + " -- " + entry.getValue());
+        }
+
+        // Navega nos registros
+        for ( String key : estudantes.keySet() ) {
+            System.out.println(key + " --- " + estudantes.get(key));
+        }
     }
 }
