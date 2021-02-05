6f3dec179d0efd2184ef5dd04f52c812554f9267
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-05 14:44:38 -0300

    Update

--- lambda/src/br/maua/Main.java
+++ lambda/src/br/maua/Main.java
@@ -3,6 +3,7 @@ package br.maua;
 public class Main {
 
     public static void main(String[] args) {
-
+        Function colocarPrefixoSenhor = valor -> "Sr. " + valor;
+        System.out.println(colocarPrefixoSenhor.gerar("Matheus"));
     }
 }
