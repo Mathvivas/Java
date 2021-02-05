f118e18607dccf94954fa115a0509331572a3f04
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-05 11:31:30 -0300

    Update funcional

--- funcional/src/br/maua/Main.java
+++ funcional/src/br/maua/Main.java
@@ -1,8 +1,29 @@
 package br.maua;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	    
+	    int[] valores = {1, 2, 3, 4};
+
+        Arrays.stream(valores)
+                .filter(valor -> valor % 2 == 0)
+                .map(valor -> valor * 2)
+                .forEach(valor -> System.out.println(valor));
+
+        System.out.println("\nOutro método\n");
+
+        for ( int i = 0; i < valores.length; i++ ) {
+            int val = 0;
+
+            if ( valores[i] % 2 == 0 ) {
+                val = valores[i] * 2;
+
+                if ( val != 0 ) {
+                    System.out.println(val);
+                }
+            }
+        }
     }
 }
