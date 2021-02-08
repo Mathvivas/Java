821239945a7854d1e1a34f2dc0b425201d06badf
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-08 16:02:15 -0300

    Update Classe de Teste

--- RealizarTestes/src/test/java/teste/CalculadoraTest.java
+++ RealizarTestes/src/test/java/teste/CalculadoraTest.java
@@ -2,10 +2,14 @@ package teste;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculadoraTest {
 
     @Test
     void testSomar() {
         Calculadora calculadora = new Calculadora();
+        int soma = calculadora.somar("1+1+3");
+        assertEquals(5, soma);          // Validação do Teste
     }
 }
