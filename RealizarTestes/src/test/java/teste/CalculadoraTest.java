ac3b1ba9e121ce8d0ca5a14ab1cd1f37b4ed08fd
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-08 17:47:36 -0300

    Teste com Mock

--- RealizarTestes/src/test/java/teste/CalculadoraTest.java
+++ RealizarTestes/src/test/java/teste/CalculadoraTest.java
@@ -1,15 +1,29 @@
 package teste;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class CalculadoraTest {
 
     @Test
-    void testSomar() {
+    public void testSomar() {
         Calculadora calculadora = new Calculadora();
         int soma = calculadora.somar("1+1+3");
         assertEquals(5, soma);          // Validação do Teste
+    }
+
+    @Test
+    public void testeSomarComMock() {
+        Calculadora calculadora = mock(Calculadora.class);
+
+        when(calculadora.somar("1+2")).thenReturn(10);  // Força o resultado de 10
+
+        int resultado = calculadora.somar("1+2");
+
+        assertEquals(10, resultado);
     }
 }
