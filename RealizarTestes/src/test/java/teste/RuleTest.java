2948523e2f264e0799a0e22c7399e1c975f06a66
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-08 16:24:59 -0300

    Update

--- RealizarTestes/src/test/java/teste/RuleTest.java
+++ RealizarTestes/src/test/java/teste/RuleTest.java
@@ -1,4 +1,26 @@
 package teste;
 
+import org.junit.jupiter.api.Test;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class RuleTest {
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Test
+    void shouldCreateNewFileInTemporaryFolder() throws IOException {
+        File created = tempFolder.newFile("file.txt");
+
+        assertTrue(created.isFile());       // Verifica se o arquivo foi criado
+        assertEquals(tempFolder.getRoot(), created.getParentFile());
+    }
 }
