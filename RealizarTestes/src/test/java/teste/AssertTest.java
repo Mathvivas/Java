cf16f05b39d269f61fbfe0b98dafda05839023bb
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-08 16:11:00 -0300

    Update Testes

--- RealizarTestes/src/test/java/teste/AssertTest.java
+++ RealizarTestes/src/test/java/teste/AssertTest.java
@@ -2,10 +2,45 @@ package teste;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AssertTest {
 
     @Test
     void testAssertArrayEquals() {
         byte[] esperado = "teste".getBytes();
+        byte[] atual = "teste".getBytes();
+        assertArrayEquals(esperado, atual);
+    }
+
+    @Test
+    void testAssertEquals() {
+        assertEquals("text", "text");
+    }
+
+    @Test
+    void testAssertFalse() {
+        assertFalse(false);
+    }
+
+    @Test
+    void testAssertNotNull() {
+        assertNotNull(new Object());
+    }
+
+    @Test
+    void testAssertNotSame() {
+        assertNotSame(new Object(), new Object());
+    }
+
+    @Test
+    void testAssertNull() {
+        assertNull(null);
+    }
+
+    @Test
+    void testAssertSame() {
+        Integer aNumber = Integer.valueOf(765);
+        assertSame(aNumber, aNumber);
     }
 }
