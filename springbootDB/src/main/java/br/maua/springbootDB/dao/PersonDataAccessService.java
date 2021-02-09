480d430fecff37ef585399d7f2528c08a5b4894d
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-09 18:00:53 -0300

    Outros Métodos

--- springbootDB/src/main/java/br/maua/springbootDB/dao/PersonDataAccessService.java
+++ springbootDB/src/main/java/br/maua/springbootDB/dao/PersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("DAO")  // Classe que acessa o banco de dados diretamente. Possui todas as operações relacionadas com DB.
@@ -21,5 +22,22 @@ public class PersonDataAccessService implements PersonDAO {
     @Override
     public List<Person> selectAllPeople() {
         return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
     }
 }
