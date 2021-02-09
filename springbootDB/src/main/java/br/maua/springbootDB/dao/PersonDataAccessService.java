b250d5e11b6f5329e7d0a3c51d2ab4fd1cbcbde0
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-09 17:31:46 -0300

    Adição de Anotações

--- springbootDB/src/main/java/br/maua/springbootDB/dao/PersonDataAccessService.java
+++ springbootDB/src/main/java/br/maua/springbootDB/dao/PersonDataAccessService.java
@@ -1,11 +1,13 @@
 package br.maua.springbootDB.dao;
 
 import br.maua.springbootDB.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("DAO")  // Classe que acessa o banco de dados diretamente. Possui todas as operações relacionadas com DB.
 public class PersonDataAccessService implements PersonDAO {
 
     private static List<Person> DB = new ArrayList<>();
--- springbootDB/src/main/java/br/maua/springbootDB/service/PersonService.java
+++ springbootDB/src/main/java/br/maua/springbootDB/service/PersonService.java
@@ -2,12 +2,17 @@ package br.maua.springbootDB.service;
 
 import br.maua.springbootDB.dao.PersonDAO;
 import br.maua.springbootDB.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service    // Classe que contém o business logic.
 public class PersonService {
 
     private final PersonDAO personDAO;
 
-    public PersonService(PersonDAO personDAO) {
+    @Autowired
+    public PersonService(@Qualifier("DAO") PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
