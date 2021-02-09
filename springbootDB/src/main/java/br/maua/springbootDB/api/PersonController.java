26680a42421882f78fa1efbe28887a60a23859ac
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-09 17:40:41 -0300

    Anotações Adicionais

--- springbootDB/src/main/java/br/maua/springbootDB/api/PersonController.java
+++ springbootDB/src/main/java/br/maua/springbootDB/api/PersonController.java
@@ -4,8 +4,11 @@ import br.maua.springbootDB.model.Person;
 import br.maua.springbootDB.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
 
@@ -17,7 +20,7 @@ public class PersonController {
     }
 
     @PostMapping    // Método HTTP POST -> Criação
-    public void addPerson(Person person) {
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 }
