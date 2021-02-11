f6cab6561e7f299ea850cdc3e83ef6cd69bd57fd
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-11 14:32:53 -0300

    Update Atividade1

--- GFTAtividade/src/br/maua/Atividade1.java
+++ GFTAtividade/src/br/maua/Atividade1.java
@@ -11,11 +11,25 @@ public class Atividade1 {
         System.out.println("\nDigite o multiplicando: ");
         int mult = scan.nextInt();
 
+        if ( (mult < 0) || (mult > 3000) ) {
+            System.out.println("\nValor Inválido! Multiplicador precisa estar entre 0 e 3000!");
+        }
+
         System.out.println("\nDigite o número incial da multiplicação: ");
         int inicio = scan.nextInt();
 
         System.out.println("\nDigite o número final da multiplicação: ");
         int fim = scan.nextInt();
+
+        int intervalo = fim - inicio;
+
+        if ( fim < inicio ) {
+            System.out.println("\nO início do intervalo deve ser menor que seu fim!");
+        }
+
+        if ( intervalo > 10 ) {
+            System.out.println("\nO intervalo não pode ter uma diferença maior que 10.");
+        }
 
         System.out.println("\nMultiplicando: " + mult);
         System.out.println("\nInício do Intervalo: " + inicio);
