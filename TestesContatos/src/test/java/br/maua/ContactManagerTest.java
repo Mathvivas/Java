1fecf10ba01073d12bb4f3a054386291911527ac
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-24 13:10:03 -0300

    Update ContactManagerTest

--- TestesContatos/src/test/java/br/maua/ContactManagerTest.java
+++ TestesContatos/src/test/java/br/maua/ContactManagerTest.java
@@ -1,13 +1,21 @@
 package br.maua;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ContactManagerTest {
 
+    ContactManager contactManager;
+
+    @BeforeEach
+    public void setup() {
+        contactManager = new ContactManager();
+    }
+
     @Test
     public void shouldCreateContact() {
-        ContactManager contactManager = new ContactManager();
         contactManager.addContact("Johnny", "Silverhand", "0123456789");
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
         Assertions.assertEquals(1, contactManager.getAllContacts().size());
@@ -17,5 +25,29 @@ public class ContactManagerTest {
                     contact.getPhoneNumber().equals("0123456789"))
             .findAny()
             .isPresent());
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When First Name is Null")
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Silverhand", "0123456789");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Last Name is Null")
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Johnny", null, "0123456789");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Phone Number is Null")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Johnny", "Silverhand", null);
+        });
     }
 }
