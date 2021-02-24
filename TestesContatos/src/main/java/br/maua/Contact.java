a9dbd5b3dc49c61a30d972a89f089bd16bb3452e
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-24 11:36:36 -0300

    Update Contact

--- TestesContatos/src/main/java/br/maua/Contact.java
+++ TestesContatos/src/main/java/br/maua/Contact.java
@@ -1,4 +1,49 @@
 package br.maua;
 
 public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Contact(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void validateFirstName() {
+        if ( this.firstName.isBlank() )
+            throw new RuntimeException("First name cannot be null or empty!");
+    }
+
+    public void validadeLastName() {
+        if ( this.lastName.isBlank() )
+            throw new RuntimeException("Last name cannot be null or empty!");
+    }
+
+    public void validatePhoneNumber() {
+        if ( this.phoneNumber.isBlank() ) {
+            throw new RuntimeException("Phone number cannot be null or empty!");
+        }
+
+        if ( this.phoneNumber.length() != 10 ) {
+            throw new RuntimeException("Phone number should be 10 digits long!");
+        }
+
+        if ( !this.phoneNumber.matches("\\d+") ) {
+            throw new RuntimeException("Phone number contains only digits!");
+        }
+
+        if ( !this.phoneNumber.startsWith("0") ) {
+            throw new RuntimeException("Phone number should starts with 0");
+        }
+    }
 }
