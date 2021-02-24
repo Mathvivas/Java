d80be23ca851672b3434ca29709377b9da6bde32
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-24 11:47:31 -0300

    Update ContactManager

--- TestesContatos/src/main/java/br/maua/ContactManager.java
+++ TestesContatos/src/main/java/br/maua/ContactManager.java
@@ -1,4 +1,36 @@
 package br.maua;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ContactManager {
+
+    Map<String, Contact> contactList = new ConcurrentHashMap<String, Contact>();
+
+    public void addContact(String firstName, String lastName, String phoneNumber) {
+        Contact contact = new Contact(firstName, lastName, phoneNumber);
+        validateContact(contact);
+        checkIfContactAlreadyExists(contact);
+        contactList.put(generateKey(contact), contact);
+    }
+
+    public Collection<Contact> getAllContacts() {
+        return contactList.values();
+    }
+
+    private void checkIfContactAlreadyExists(Contact contact) {
+        if ( contactList.containsKey(generateKey(contact)) )
+            throw new RuntimeException("Contact Already Exists!");
+    }
+
+    private void validateContact(Contact contact) {
+        contact.validateFirstName();
+        contact.validadeLastName();
+        contact.validatePhoneNumber();
+    }
+
+    private String generateKey(Contact contact) {
+        return String.format("%s-%s", contact.getFirstName(), contact.getLastName());
+    }
 }
