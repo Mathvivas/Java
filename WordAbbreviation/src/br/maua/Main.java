f9abf424ae39d110196a352745bebdc9ec7826bb
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-25 14:56:29 -0300

    Update

--- WordAbbreviation/src/br/maua/Main.java
+++ WordAbbreviation/src/br/maua/Main.java
@@ -1,5 +1,7 @@
 package br.maua;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         String[] palavras;
         String frase;
-        String[] utilizados;
+        List<Character> utilizados = new ArrayList<Character>();
 
         Scanner scan = new Scanner(System.in);
 
@@ -17,7 +19,15 @@ public class Main {
         palavras = frase.split(" ");
 
         for ( String palavra : palavras ) {
-            System.out.println(palavra);
+            if (!utilizados.contains(palavra.charAt(0))) { // Se o array não contém a primeira letra da palavra
+                System.out.print(palavra.charAt(0) + " ");
+                utilizados.add(palavra.charAt(0));
+
+            } else {
+                System.out.print(palavra);
+            }
         }
+
+        //System.out.println(palavras[0].charAt(0));
     }
 }
