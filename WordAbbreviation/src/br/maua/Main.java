8403df6e0df81313449cebe475236cc9691e09e9
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-25 14:29:32 -0300

    Update

--- WordAbbreviation/src/br/maua/Main.java
+++ WordAbbreviation/src/br/maua/Main.java
@@ -1,8 +1,23 @@
 package br.maua;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] palavras;
+        String frase;
+        String[] utilizados;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite uma frase a ser abreviada: ");
+        frase = scan.nextLine();
+
+        palavras = frase.split(" ");
+
+        for ( String palavra : palavras ) {
+            System.out.println(palavra);
+        }
     }
 }
