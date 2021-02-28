00f75063481b2909e1c1c2a2d8d41c2c10744e61
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-02-28 10:57:49 -0300

    Update

---
+++ HeroesApi/src/test/java/br/maua/HeroesApi/HeroesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.maua.HeroesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
