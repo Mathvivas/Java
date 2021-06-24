d97e38c543d90fda2cd7c9e3f202b88340fc9623
Mathvivas <mathvivas@hotmail.com>
2021-06-24 15:50:08 -0300

    Restrições

--- EstagioDesafioTecnico/src/br/maua/Main.java
+++ EstagioDesafioTecnico/src/br/maua/Main.java
@@ -1,8 +1,19 @@
 package br.maua;
 
+import br.maua.enums.Produto;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\nDigite um código de barras: ");
+
+        Scanner scan = new Scanner(System.in);
+
+//        System.out.println("\nDigite um código de barras: ");
+//        String codigo = scan.nextLine();
+
+
     }
 }
--- EstagioDesafioTecnico/src/br/maua/classes/ValidadorDeCodigo.java
+++ EstagioDesafioTecnico/src/br/maua/classes/ValidadorDeCodigo.java
@@ -1,5 +1,12 @@
 package br.maua.classes;
 
+import br.maua.enums.Produto;
+import br.maua.enums.Regiao;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Esta classe permite realizar a validação do código
  * de barras de acordo com as especificações impostas
@@ -9,9 +16,34 @@ package br.maua.classes;
 public class ValidadorDeCodigo {
 
     public static final Integer TAMANHO_CODIGO = 15;
+    public static final String VENDEDOR_INVALIDO = "584";
 
-    public void validar() {
+    public void validar(String codigo) {
 
+        String[] codigos = codigo.split("(?<=\\G.{3})");
 
+        Produto[] produtos;
+        produtos = (Produto[]) Arrays.stream(Produto.values()).toArray();
+        Regiao[] regioes;
+        regioes = (Regiao[]) Arrays.stream(Regiao.values()).toArray();
+
+        // Verificando o código do vendedor (código 584 é inválido)
+        if ( codigos[3].equals(VENDEDOR_INVALIDO) ) {
+            System.out.println("\nCódigo do Vendedor é Inválido!");
+        }
+
+        if ( codigos[1].equals(Regiao.CENTROOESTE.getCodigo()) && codigos[0].equals(Produto.JOIAS.getCodigo()) ) {
+            System.out.println("\nNão é possível despachar pacotes contendo jóias vindo do Centro-Oeste!");
+        }
+
+        // Checando se os Produtos possuem código diferente do permitido
+        List<String> produtosValidos = new ArrayList<>();
+        for (Produto prod : produtos) {
+            produtosValidos.add(prod.getCodigo());
+        }
+
+        if ( !produtosValidos.contains(codigos[4]) ) {
+            System.out.println("\nProduto não permitido!");
+        }
     }
 }
