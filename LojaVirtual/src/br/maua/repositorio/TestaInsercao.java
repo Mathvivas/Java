4f02a70666ceebe25ed7a2e199db869fc2817032
Mathvivas <mathvivas@hotmail.com>
2021-06-28 15:55:12 -0300

    Implementação da Inserção e da Remoção de dados

---
+++ LojaVirtual/src/br/maua/repositorio/TestaInsercao.java
@@ -0,0 +1,31 @@
+package br.maua.repositorio;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+
+    public static void main(String[] args) throws SQLException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.recuperarConexao();
+
+        Statement stm = connection.createStatement();
+        stm.execute("INSERT INTO produto (nome, descricao) VALUES ('Mouse', 'Mouse sem fio')",
+                Statement.RETURN_GENERATED_KEYS);
+
+        /*
+            Método execute devolve true quando seu resultado é um java.sql.ResultSet
+            e false caso contrário (update, delete, insert)
+         */
+
+
+        ResultSet rst = stm.getGeneratedKeys();
+        while ( rst.next() ) {
+            int id = rst.getInt(1);
+            System.out.println("\nO id criado foi: " + id);
+        }
+    }
+}
---
+++ LojaVirtual/src/br/maua/repositorio/TestaRemocao.java
@@ -0,0 +1,20 @@
+package br.maua.repositorio;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaRemocao {
+
+    public static void main(String[] args) throws SQLException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.recuperarConexao();
+
+        Statement stm = connection.createStatement();
+        stm.execute("DELETE FROM produto WHERE id = 3");
+
+        int linhasModificadas = stm.getUpdateCount();
+        System.out.println("Quantidade de linhas modificadas: " + linhasModificadas);
+    }
+}
