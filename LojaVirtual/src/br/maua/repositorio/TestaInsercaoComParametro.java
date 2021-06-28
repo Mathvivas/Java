d63bfb6cf699117f0672f622e68e303c3adeca07
Mathvivas <mathvivas@hotmail.com>
2021-06-28 17:13:16 -0300

    Evitando SQL Injection

--- LojaVirtual/src/br/maua/repositorio/TestaInsercaoComParametro.java
+++ LojaVirtual/src/br/maua/repositorio/TestaInsercaoComParametro.java
@@ -1,31 +1,24 @@
 package br.maua.repositorio;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TestaInsercaoComParametro {
 
     public static void main(String[] args) throws SQLException {
 
-        String nome = "";
-        String descricao = "";
+        String nome = "Mouse";
+        String descricao = "Mouse sem fio";
 
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.recuperarConexao();
 
-        String sql = "INSERT INTO produto (nome, descricao) VALUES ('" + nome + "', '" + descricao + "')";
+        PreparedStatement stm = connection.prepareStatement(
+                "INSERT INTO produto (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS
+        );
 
-        Statement stm = connection.createStatement();
-        stm.execute(sql,
-                Statement.RETURN_GENERATED_KEYS);
-
-        /*
-            Método execute devolve true quando seu resultado é um java.sql.ResultSet
-            e false caso contrário (update, delete, insert)
-         */
-
+        stm.setString(1, nome);
+        stm.setString(2, descricao);
+        stm.execute();
 
         ResultSet rst = stm.getGeneratedKeys();
         while ( rst.next() ) {
