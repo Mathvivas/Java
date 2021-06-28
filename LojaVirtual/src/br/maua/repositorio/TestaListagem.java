60458ea79f46d62073797f27b3376502951b54be
Mathvivas <mathvivas@hotmail.com>
2021-06-28 15:25:02 -0300

    Testando a conexão com o Banco de Dados MySQL

---
+++ LojaVirtual/src/br/maua/repositorio/TestaListagem.java
@@ -0,0 +1,28 @@
+package br.maua.repositorio;
+
+import java.sql.*;
+
+public class TestaListagem {
+    public static void main(String[] args) throws SQLException {
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = connectionFactory.recuperarConexao();
+
+        Statement stm = connection.createStatement();
+        stm.execute("SELECT id, nome, descricao FROM produto");
+
+        ResultSet rst = stm.getResultSet();
+
+        while ( rst.next() ) {
+            Integer id = rst.getInt("id");
+            System.out.println(id);
+            String nome = rst.getString("nome");
+            System.out.println(nome);
+            String descricao = rst.getString("descricao");
+            System.out.println(descricao);
+        }
+
+        System.out.println("\nFechando conexão.");
+        connection.close();
+    }
+}
