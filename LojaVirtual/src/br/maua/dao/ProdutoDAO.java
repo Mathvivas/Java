b9ee77ff9479d7b8d99aefcc1b3f23f6312f037e
Mathvivas <mathvivas@hotmail.com>
2021-06-29 14:05:14 -0300

    Melhoria com Data Access Object

---
+++ LojaVirtual/src/br/maua/dao/ProdutoDAO.java
@@ -0,0 +1,51 @@
+package br.maua.dao;
+
+import br.maua.modelo.Produto;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAO {
+
+    private Connection connection;
+
+    public ProdutoDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void salvar(Produto produto) throws SQLException {
+        String sql = "INSERT INTO produto (nome, descricao) VALUES (?, ?)";
+
+        try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstm.setString(1, produto.getNome());
+            pstm.setString(2, produto.getDescricao());
+            pstm.execute();
+
+            try (ResultSet rst = pstm.getGeneratedKeys()) {
+                while ( rst.next() ) {
+                    produto.setId(rst.getInt(1));
+                }
+            }
+        }
+    }
+
+    public List<Produto> listar() throws SQLException {
+        List<Produto> produtos = new ArrayList<>();
+
+        String sql = "SELECT id, nome, descricao FROM produto";
+
+        try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+            pstm.execute();
+
+            try (ResultSet rst = pstm.getResultSet()) {
+                while ( rst.next() ) {
+                    Produto produto = new Produto(rst.getInt(1), rst.getString(2), rst.getString(3));
+
+                    produtos.add(produto);
+                }
+            }
+        }
+        return produtos;
+    }
+}
