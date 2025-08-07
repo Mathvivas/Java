5c199193721ee84146bc54503a75e931459ce1bd
Mathvivas <mathvivas@hotmail.com>
2025-08-07 16:11:54 -0300

    Criação das classes de Paciente

---
+++ rest_api/src/main/java/com/matheus/api/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,13 @@
+package com.matheus.api.paciente;
+
+import com.matheus.api.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco
+) {
+}
