1d0e52707830c01a023fa23ec94ebdf0386d24ea
Mathvivas <mathvivas@hotmail.com>
2025-08-07 21:45:04 -0300

    Criação de um método que devolve todas as informações de um médico

--- rest_api/src/main/java/com/matheus/api/controller/MedicoController.java
+++ rest_api/src/main/java/com/matheus/api/controller/MedicoController.java
@@ -60,6 +60,12 @@ public class MedicoController {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+
 //    @GetMapping
 //    public List<DadosListagemMedico> listar() {
 //        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
